028af1e7628855173bc587234d28a382ccf8d38d
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2021-07-07 12:44:48 +0200

    Add basic icons

---
+++ src/helpers/iconsTypes.js
@@ -0,0 +1,15 @@
+import { ReactComponent as FolderIcon } from '../assets/icons/folder.svg';
+import { ReactComponent as LogoIcon } from '../assets/icons/Logo.svg';
+import { ReactComponent as ChartIcon } from '../assets/icons/pie-chart.svg';
+import { ReactComponent as SettingsIcon } from '../assets/icons/settings-1.svg';
+import { ReactComponent as SettingsAltIcon } from '../assets/icons/settings.svg';
+
+const iconsTypes = {
+  FolderIcon,
+  LogoIcon,
+  ChartIcon,
+  SettingsAltIcon,
+  SettingsIcon,
+};
+
+export default iconsTypes;
